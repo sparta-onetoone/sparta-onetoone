f021d2e944fdcf6a1785a60617a7d904a51df8ca
venus01 <geumsung7769@naver.com>
2025-02-15 19:02:53 +0900

    feat : QueryDslConfig 내부 구현

--- src/main/java/com/eureka/spartaonetoone/common/config/QueryDslConfig.java
+++ src/main/java/com/eureka/spartaonetoone/common/config/QueryDslConfig.java
@@ -1,7 +1,15 @@
 package com.eureka.spartaonetoone.common.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
