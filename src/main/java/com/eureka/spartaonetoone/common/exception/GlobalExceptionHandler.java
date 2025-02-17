9e6ab95b8aa182d6ef5cf148e6519f60cf4b68ed
venus01 <geumsung7769@naver.com>
2025-02-17 15:28:40 +0900

    fix : 예외 처리기에 로깅 추가
    예외 처리기에 로깅, customExceptionHandle에
    설정한 @Order 제거

--- src/main/java/com/eureka/spartaonetoone/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eureka/spartaonetoone/common/exception/GlobalExceptionHandler.java
@@ -2,24 +2,25 @@ package com.eureka.spartaonetoone.common.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.core.annotation.Order;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<?> dataIntegrityViolationExceptionHandle(final DataIntegrityViolationException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorMessage.of(ex.getMessage(), "DB-001"));
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorMessage.of("잘못된 요청입니다.", "RQ-001"));
     }
 
-    @Order(1)
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<?> customExceptionHandle(CustomException ex) {
         return ResponseEntity
