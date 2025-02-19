b8cf86b8e61b9e113f71ad3fb832677af82277ef
Jieon <dlwldjs7544@naver.com>
2025-02-19 14:31:22 +0900

    test : 장바구니 관련 기능 단위 테스트

--- src/main/java/com/eureka/spartaonetoone/domain/cart/application/dtos/request/CartItemUpdateRequestDto.java
+++ src/main/java/com/eureka/spartaonetoone/domain/cart/application/dtos/request/CartItemUpdateRequestDto.java
@@ -1,13 +1,13 @@
 package com.eureka.spartaonetoone.domain.cart.application.dtos.request;
 
-import java.util.UUID;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CartItemUpdateRequestDto {
 	@NotNull
 	@JsonProperty("quantity")
