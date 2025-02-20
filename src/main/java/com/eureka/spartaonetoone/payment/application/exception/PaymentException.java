b3ecf435442bac8f120654faebc12902318357e7
venus01 <geumsung7769@naver.com>
2025-02-20 20:21:45 +0900

    feat : 도메인들 간 통신을 위해 사용하는 PaymentClient 개발
    도메인들 간 통신을 위해 사용하는 PaymentClient 개발하였음,
    디렉토리 구조 변경으로 인하여 Payment 도메인과 관련된 파일들의 패키지 경로 수정

--- src/main/java/com/eureka/spartaonetoone/payment/application/exception/PaymentException.java
+++ src/main/java/com/eureka/spartaonetoone/payment/application/exception/PaymentException.java
@@ -1,4 +1,4 @@
-package com.eureka.spartaonetoone.domain.payment.application.exception;
+package com.eureka.spartaonetoone.payment.application.exception;
 
 import com.eureka.spartaonetoone.common.exception.CustomException;
 import org.springframework.http.HttpStatus;
