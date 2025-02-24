3785c572f41e58cf4f4d4c317e65e7690993f407
venus01 <geumsung7769@naver.com>
2025-02-25 03:06:25 +0900

    feat : 결제내역에 대한 접근을 ROLE에 따라 분기하는 설정을 추가하였음
    결제내역에 대한 접근을 ROLE에 따라 분기하는 설정을 추가하였음.
    결제내역에 접근하려는 사용자의 정보를 얻기 위하여 사용되는 클라이언트들을
    추가하였음.

--- src/main/java/com/eureka/spartaonetoone/common/client/OrderClient.java
+++ src/main/java/com/eureka/spartaonetoone/common/client/OrderClient.java
@@ -1,9 +1,12 @@
 package com.eureka.spartaonetoone.common.client;
 
 import com.eureka.spartaonetoone.common.dtos.response.OrderResponse;
+import com.eureka.spartaonetoone.common.utils.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -11,29 +14,38 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderClient {
 
-	// 외부 주문 API와 통신하기 위한 WebClient 빈
-	private final WebClient webClient;
+    // 주문 서비스의 API 엔드포인트를 상수로 정의 (예: "/api/v1/orders")
+    private static final String ORDERS_URI = "/api/v1/orders";
+    // 외부 주문 API와 통신하기 위한 WebClient 빈
+    private final WebClient webClient;
 
-	// 주문 서비스의 API 엔드포인트를 상수로 정의 (예: "/api/v1/orders")
-	private static final String ORDERS_URI = "/api/v1/orders";
+    public List<OrderResponse> getOrders(UUID storeId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(ORDERS_URI)
+                        .queryParam("store_id", storeId)
+                        .build())
+                .retrieve()
+                .bodyToFlux(OrderResponse.class)
+                .collectList()
+                .block();
+    }
 
-	public List<OrderResponse> getOrders(UUID storeId) {
-		return webClient.get()
-			.uri(uriBuilder -> uriBuilder
-				.path(ORDERS_URI)
-				.queryParam("store_id", storeId)
-				.build())
-			.retrieve()
-			.bodyToFlux(OrderResponse.class)
-			.collectList()
-			.block();
-	}
+    public UUID getStoreIdOfOrder(UUID orderId) {
+        OrderResponse.GetStoreId getStoreId = webClient.get()
+                .uri(ORDERS_URI + "/{order_id}", orderId)
+                .retrieve()
+                .bodyToMono(
+                        new ParameterizedTypeReference<CommonResponse<OrderResponse.GetStoreId>>() {
+                        }).map(CommonResponse::getData)
+                .block();
 
-	public OrderResponse.GetStoreId getStoreIdOfOrder(UUID orderId) {
-		return webClient.get()
-			.uri(ORDERS_URI + "/{order_id}", orderId)
-			.retrieve()
-			.bodyToMono(OrderResponse.GetStoreId.class)
-			.block();
-	}
+        return getStoreId.getStoreId();
+//                .bodyToMono(String.class)
+//                .block();
+//		return string;
+//			.bodyToMono(OrderResponse.GetStoreId.class)
+//			.block();
+//        return null;
+    }
 }
